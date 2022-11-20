99e3ee7fd72396481fee2bb7266169d70df156d4
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-11-19 23:57:18 -0300

    chore: first commit

---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "aws_ses_domain_identity" "this" {
+  provider = aws.current
+  domain = var.domain
+}
+
+resource "aws_ses_domain_dkim" "this" {
+  provider = aws.current
+  domain = aws_ses_domain_identity.this.domain
+}
+
+resource "aws_ses_email_identity" "this" {
+  provider = aws.current
+  email = var.email_identity
+}
+
+module "dns_txt_record" {
+  source = "github.com/ptonini/terraform-aws-route53-record?ref=v1"
+  route53_zone = var.route53_zone
+  name = "_amazonses.nodis.com.br"
+  type = "TXT"
+  records = [
+    aws_ses_domain_identity.this.verification_token
+  ]
+  providers = {
+    aws = aws.dns
+  }
+}
+
+module "dns_cname_record" {
+  source = "github.com/ptonini/terraform-aws-route53-record?ref=v1"
+  count = 3
+  route53_zone = var.route53_zone
+  name = "${element(aws_ses_domain_dkim.this.dkim_tokens, count.index)}._domainkey.nodis.com.br"
+  type = "CNAME"
+  ttl = "600"
+  records = [
+    "${element(aws_ses_domain_dkim.this.dkim_tokens, count.index)}.dkim.amazonses.com"
+  ]
+  providers = {
+    aws = aws.dns
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "domain" {}
+
+variable "email_identity" {}
+
+variable "route53_zone" {}
